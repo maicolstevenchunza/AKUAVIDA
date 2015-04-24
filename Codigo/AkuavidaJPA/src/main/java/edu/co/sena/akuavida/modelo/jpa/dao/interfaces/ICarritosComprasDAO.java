0dc4f72c4bc6c6d6a6738f0cf4247fa6dc42ed4b
maicolstevenchunza <mschunza@misena.edu.co>
2015-04-23 19:49:17 -0500

    Nuevo codigo Chunza

---
+++ Codigo/AkuavidaJPA/src/main/java/edu/co/sena/akuavida/modelo/jpa/dao/interfaces/ICarritosComprasDAO.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.co.sena.akuavida.modelo.jpa.dao.interfaces;
+
+import edu.co.sena.akuavida.modelo.entitis.CarritoDeCompras;
+import java.util.List;
+
+/**
+ *
+ * @author ColsutecR
+ */
+public interface ICarritosComprasDAO {
+    
+    public void insert(CarritoDeCompras entity);
+    public void update(CarritoDeCompras entity);
+    public void delete(CarritoDeCompras entity);
+    
+    public CarritoDeCompras findByIDCarrito();
+    
+    public List<CarritoDeCompras> findByAll();
+    public List<CarritoDeCompras> findByTotal(Object total);
+    public List<CarritoDeCompras> findBySubtotal(Object subtotal);        
+}
